9572708c61a30cffef9eac255cf2be896d6b65f5
zKay <ductung.ptit@gmail.com>
2026-01-31 03:35:30 +0700

    Scene Level

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
@@ -6,10 +7,17 @@ using UnityEngine.SceneManagement;
 public class LevelManager : Singleton<LevelManager>
 {
     [SerializeField] private List<SceneAsset> levels;
+    private bool[] _unlockedLevels = new bool[11];
     protected override void Awake()
     {
         base.Awake();
         DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        _unlockedLevels[0] = true;
+        _unlockedLevels[1] = true;
     }
 
     public void Load(int index)
