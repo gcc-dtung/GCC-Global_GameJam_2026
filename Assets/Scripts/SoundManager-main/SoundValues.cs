95e388979bf79eb72067a7c2a591ddb422ff9c7a
plantcraft94 <plantcraft60@gmail.com>
2026-01-31 15:59:49 +0700

    music

--- Assets/Scripts/SoundManager-main/SoundValues.cs
+++ Assets/Scripts/SoundManager-main/SoundValues.cs
@@ -3,8 +3,10 @@
 
 namespace SmallHedge.SoundManager
 {
-    public enum SoundType
-    {
-        //Put sound names in here
-    }
+	public enum SoundType
+	{
+		BackgroundSound,
+		UIButtonClick,
+		
+	}
 }
